c8ba2c835033276f967f4f41bd847b98f2a0812a
JBY <qpv008@gmail.com>
2022-07-12 18:48:09 +0900

    0712

--- login_re/js/menu.js
+++ login_re/js/menu.js
@@ -7,37 +7,7 @@ $(function(){
     $('.category_menu_wrap').on('click',function(){
         $('.all_menu').css("display","block");
     })
-    $('#sub_menu_wrap').hide();
-
-    // $('#menu .menu .main_menu ul a li:eq(0)').mouseenter(function(){
-    //     $('#digital_print_sub').slideDown(1000);
-    // });
-    // $('#menu .menu .main_menu ul a li:eq(1)').mouseenter(function(){
-    //     $('#high_print_sub').slideDown(1000);
-    // });
-    // $('#menu .menu .main_menu ul a li:eq(2)').mouseenter(function(){
-    //     $('#commercial_print_sub').slideDown(1000);
-    // });
-    // $('#menu .menu .main_menu ul a li:eq(3)').mouseenter(function(){
-    //     $('#mypage_sub').slideDown(1000);
-    // });
-    // $('#menu .menu .main_menu ul a li:eq(4)').mouseenter(function(){
-    //     $('#help_sub').slideDown(1000);
-    // });
-    // $('#menu .menu .main_menu ul a li:eq(5)').mouseenter(function(){
-    //     $('#event_sub').slideDown(1000);
-    // });
-    // // $('#menu .menu .main_menu ul a li:eq(0)').mouseleave(function(){
-    // //     $('.sub_menu_wrap').css('display','none')
-    // // });
-    $('.sub_menu_wrap').mouseenter(function(){
-    });
-    $('.sub_menu_wrap #sub_menu_rest').mouseenter(function(){
-        // $('#digital_print_sub').slideUp(1000);
-        // $('#high_print_sub').slideUp(1000);
-        // $('#commercial_print_sub').slideUp(1000);
-        // $('#mypage_sub').slideUp(1000);
-        // $('#help_sub').slideUp(1000);
-        // $('#event_sub').slideUp(1000);
-    });
+    let menu_height = $('#menu').outerHeight();
+    $('.sub_menu_wrapper').css('top', menu_height);
+    $('.bg_dark').css('top', menu_height);
 })
