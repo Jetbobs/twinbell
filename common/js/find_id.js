83aa5eb2360d007da6089a379ad8741e0cb556ef
JBY <qpv008@gmail.com>
2022-07-15 10:15:16 +0900

    0715

---
+++ common/js/find_id.js
@@ -0,0 +1,8 @@
+$(function(){
+    if(!(isCorrect($('#name_input input').val()))){
+        $('#name_input input').val('');
+    };
+    if(!(isPhoneNumber($('#phone_input input').val()))){
+        
+    };
+});
